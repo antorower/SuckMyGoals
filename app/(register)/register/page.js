33be8f4329d12bb4ba427f73b912dc62e19d63fa
antorower <antonis.mastorakis@gmail.com>
2024-08-16 18:45:55 +0300

    register form fixed

--- app/(register)/register/page.js
+++ app/(register)/register/page.js
@@ -1,10 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { currentUser } from "@clerk/nextjs/server";
 import { RegisterUser } from "@/lib/RegisterActions";
 import { RegisterOwner } from "@/lib/RegisterActions";
+import { notFound } from "next/navigation";
 
 const Register = async () => {
+  const [isButtonActive, setIsButtonActive] = useState(true);
   const clerkUser = await currentUser();
+  if (!clerkUser) notFound();
+
   return (
     <form action={clerkUser?.publicMetadata.owner ? RegisterOwner : RegisterUser} autoComplete="none" className="flex flex-col w-full max-w-[300px]">
       <input type="text" required pattern="[A-Za-z]+" title="First name should only contain letters." minLength={3} maxLength={20} name="firstName" autoComplete="none" placeholder="First Name" className={`${inputClasses} border-t border-x rounded-t`} />
@@ -12,7 +17,7 @@ const Register = async () => {
       <input type="tel" title="Telephone should only contain numbers and special characters like +, -, (, )." minLength={3} maxLength={20} name="telephone" autoComplete="none" placeholder="Telephone" className={`${inputClasses} border-t border-x`} />
       <input type="email" minLength={3} maxLength={50} name="bybitEmail" autoComplete="none" placeholder="Bybit Email" className={`${inputClasses} border-t border-x`} />
       <input type="text" required minLength={3} maxLength={20} name="bybitUid" autoComplete="none" placeholder="Bybit Uid" className={`${inputClasses} border`} />
-      <button type="submit" required className="bg-orange-700 p-3 rounded-b hover:bg-orange-600 text-white font-semibold outline-none">
+      <button onClick={() => setIsButtonActive(false)} disabled={!isButtonActive} type="submit" required className="bg-orange-700 p-3 rounded-b hover:bg-orange-600 text-white font-semibold outline-none">
         Register
       </button>
     </form>
