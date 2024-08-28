23c4c8d75f0de50d3e28968dda903bb6d06ce7fa
antorower <antonis.mastorakis@gmail.com>
2024-08-28 11:43:14 +0300

    accounts

--- app/(admin)/accounts/page.js
+++ app/(admin)/accounts/page.js
@@ -1,8 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
+import { GetAllAccountsLight } from "@/lib/AccountActions";
 
-const Accounts = () => {
-  return <div>Accounts</div>;
+const Accounts = async ({ searchParams }) => {
+  const accounts = await GetAllAccountsLight();
+
+  const sort = searchParams.sort || "default";
+
+  if (sort === "trade") {
+    accounts.sort((a, b) => {
+      const dateA = new Date(a.eventsTimestamp.firstTradeDate);
+      const dateB = new Date(b.eventsTimestamp.firstTradeDate);
+      return dateA - dateB; // Ascending order
+    });
+  }
+
+  if (sort === "balance") {
+    accounts.sort((a, b) => {
+      if (b.phaseWeight !== a.phaseWeight) {
+        return b.phaseWeight - a.phaseWeight; // Sort by phaseWeight, higher first
+      }
+      return b.balance - a.balance; // If phaseWeight is equal, sort by balance, higher first
+    });
+  }
+
+  if (sort === "company") {
+    accounts.sort((a, b) => {
+      if (a.company < b.company) return -1;
+      if (a.company > b.company) return 1;
+      return 0;
+    });
+  }
+
+  return (
+    <div className="flex flex-wrap gap-4 p-8 items-start">
+      {accounts.map((account) => (
+        <div className="border border-gray-800 px-3 py-2" key={account._id}>
+          <div>{account.number}</div>
+          <div>{account.balance}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Accounts;
--- components/User/Accounts/WaitingPayoutAccountCard.js
+++ components/User/Accounts/WaitingPayoutAccountCard.js
@@ -3,9 +3,17 @@ import Link from "next/link";
 
 const WaitingPayoutAccountCard = async ({ account }) => {
   const company = Companies.find((company) => company.name === account.company);
+  const day = account?.payoutRequestDate?.day;
+  const month = account?.payoutRequestDate?.month;
+
+  const today = new Date();
+  const todayDay = today.getDate();
+  const todayMonth = today.getMonth() + 1;
+
+  const isPayoutDay = day === todayDay && month === todayMonth;
 
   return (
-    <Link href={`/account?account=${account._id.toString()}`} className="border border-orange-500 shadow-2xl shadow-orange-950 rounded p-4 flex flex-col items-center justify-center gap-2 max-w-[250px]">
+    <Link href={`/account?account=${account._id.toString()}`} className={`${isPayoutDay ? "animate-pulse" : null} border border-orange-500 shadow-2xl shadow-orange-950 rounded p-4 flex flex-col items-center justify-center gap-2 max-w-[250px]`}>
       <div className="text-gray-500">Payout Account</div>
       <div className="">{company.name}</div>
       <div className="flex items-center justify-between">Profit: ${account.balance - account.capital}</div>
