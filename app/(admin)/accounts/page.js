c9d4afabfba8dd9d6ab6d9a28278ea46669a5485
antorower <antonis.mastorakis@gmail.com>
2024-09-17 19:59:39 +0300

    manager fixed

--- app/(admin)/accounts/page.js
+++ app/(admin)/accounts/page.js
@@ -106,7 +106,7 @@ const Accounts = async ({ searchParams }) => {
       <div className="flex flex-wrap gap-4 p-8 justify-center items-start">
         {accounts.map((account) => (
           <>
-            {account.status !== "Review" && (
+            {account.status !== "Review" && !manageAccountActivation && (
               <Link href={`/account?account=${account._id.toString()}`} className={`${account.status === "Review" ? "animate-pulse" : null} border text-center px-3 py-2 ${account.balance > account.capital ? "border-green-600" : null} ${account.balance < account.capital ? "border-red-600" : null} ${account.balance === account.capital ? "border-gray-800" : null}`} key={account._id}>
                 <div className={`${account.phaseWeight === 1 ? "text-blue-500" : null} ${account.phaseWeight === 2 ? "text-violet-500" : null} ${account.phaseWeight === 3 ? "text-orange-500" : null}`}>{account.company}</div>
                 <div className="text-gray-600">{account.number}</div>
@@ -115,8 +115,19 @@ const Accounts = async ({ searchParams }) => {
                 <div>{new Date(account.eventsTimestamp.firstTradeDate).toLocaleDateString("el-GR")}</div>
                 {account.eventsTimestamp.targetReachedDate && <div>{new Date(account.eventsTimestamp.targetReachedDate).toLocaleDateString("el-GR")}</div>}
                 <div>{account.balance}</div>
-                {account.company === "Funded Next" && manageAccountActivation && <ManageAccountTrades accountId={account._id.toString()} disabled={account.tradesDisabled} />}
               </Link>
+            )}
+            {account.status !== "Review" && manageAccountActivation && account.phaseWeight === 1 && account.company === "Funded Next" && (
+              <div href={`/account?account=${account._id.toString()}`} className={`${account.status === "Review" ? "animate-pulse" : null} border text-center px-3 py-2 ${account.balance > account.capital ? "border-green-600" : null} ${account.balance < account.capital ? "border-red-600" : null} ${account.balance === account.capital ? "border-gray-800" : null}`} key={account._id}>
+                <div className={`${account.phaseWeight === 1 ? "text-blue-500" : null} ${account.phaseWeight === 2 ? "text-violet-500" : null} ${account.phaseWeight === 3 ? "text-orange-500" : null}`}>{account.company}</div>
+                <div className="text-gray-600">{account.number}</div>
+                {account.status !== "Review" && <div className="text-gray-600">{account.status}</div>}
+                {account.status === "Review" && clerkUser.publicMetadata.owner && <AccountLostButton accountId={account._id.toString()} />}
+                <div>{new Date(account.eventsTimestamp.firstTradeDate).toLocaleDateString("el-GR")}</div>
+                {account.eventsTimestamp.targetReachedDate && <div>{new Date(account.eventsTimestamp.targetReachedDate).toLocaleDateString("el-GR")}</div>}
+                <div>{account.balance}</div>
+                <ManageAccountTrades accountId={account._id.toString()} disabled={account.tradesDisabled} />
+              </div>
             )}
             {account.status === "Review" && (
               <div className={`animate-pulse border text-center px-3 py-2 border-yellow-400`} key={account._id}>
