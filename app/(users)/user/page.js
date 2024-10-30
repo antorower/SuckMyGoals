9f5418df0b8ac283955c5b8e1f70308d4511bc89
antorower <antonis.mastorakis@gmail.com>
2024-10-30 14:20:38 +0200

    stats ready

--- app/(users)/user/page.js
+++ app/(users)/user/page.js
@@ -47,6 +47,9 @@ const User = async ({ searchParams }) => {
       )}
       {(admin || owner) && <RelatedUser userId={user._id.toString()} relatedUserFirstName={user.relatedUser?.firstName} />}
       <UserNote userId={user._id.toString()} note={user.note} />
+      <div className="w-full text-center text-blue-500">
+        <Link href="/stats"> Click here to see our stats </Link>
+      </div>
       {dayNote && dayNote !== "" && <div className="text-2xl flex justify-center animate-pulse bg-red-600 w-full p-4 font-semibold">{dayNote}</div>}
       {(admin || owner) && <ManageCompanies userId={user._id.toString()} activeCompanies={user.activeCompanies} admin={admin} owner={owner} />}
       <Accounts accounts={user.accounts} selectedAccounts={searchParams.accounts?.toLowerCase()} userId={user._id.toString()} admin={admin} owner={owner} />
